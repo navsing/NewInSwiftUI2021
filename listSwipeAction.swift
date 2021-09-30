88e968c0990434bc5d74b447422bd70ae5fb5989
navsing <navdeep_singh02@outlook.com>
2021-09-29 18:10:42 -0700

    Swipe action on List Items

---
+++ listSwipeAction.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+struct ListSwipeAction: View {
+    var body: some View {
+        List {
+            ForEach(1..<25) { _ in 
+                Text("List Item")
+                //Add Swipe Actions here
+                .swipeActions(edge: .trailing, allowsFullSwipe: false) {
+                    Button(action: {}) {
+                        Text("Like")
+                        //Can also add images
+                        // Image(systemName: "heart.fill")
+                    }.tint(.red)
+                    //change color of action button here
+                }
+            }
+        }    
+    }
+}
